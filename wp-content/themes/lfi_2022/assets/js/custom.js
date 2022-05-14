4b81e10e0c2b0ca2dccd784121e1b872c5cec93e
guirauo <guirauo@ssh01.cluster030.gra.hosting.ovh.net>
2022-05-14 04:47:42 +0000

    update elem pro, cors settings, js fix

--- wp-content/themes/lfi_2022/assets/js/custom.js
+++ wp-content/themes/lfi_2022/assets/js/custom.js
@@ -1,12 +1,15 @@
-let productsImage = document.querySelectorAll('.woocommerce ul.products li.product a img');
+// let productsImage = document.querySelectorAll('.woocommerce ul.products li.product a img');
 
-productsImage.forEach(item => {
-    item.addEventListener('click', function(event) {
-        event.preventDefault();
-        item.parentNode.parentNode.children[1].click();
-    });
-});
+// productsImage.forEach(item => {
+//     item.addEventListener('click', function(event) {
+//         event.preventDefault();
+//         item.parentNode.parentNode.children[1].click();
+//     });
+// });
 
 let addToCartButton = document.querySelector('.cart .single_add_to_cart_button.button.alt');
 
-addToCartButton.innerHTML = "Faire un don";
+if (addToCartButton) {
+	addToCartButton.innerHTML = "Faire un don";
+}
+
